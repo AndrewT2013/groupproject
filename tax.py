7d3e4ccc087ea819137493f237b4e2b83d60a9d8
athenacyee <89562685+athenacyee@users.noreply.github.com>
2022-05-04 12:18:25 -0700

    Add files via upload

---
+++ tax.py
@@ -0,0 +1,9 @@
+import taxjar
+
+client = taxjar.Client(api_key='bccb6325e7bc94c6da498afcf46b5564')
+
+
+def taxrate(zipcode: str) -> float:
+    rates = client.rates_for_location(zipcode)
+    print(rates.combined_rate)
+    return rates.combined_rate
